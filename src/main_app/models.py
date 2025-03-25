116a6d94a1b9f5f45a97575e58ebdaffe75d5815
Robin <jochem-robin@hotmail.fr>
2025-03-25 11:37:26 +0100

    Modifying database models in the models.py file

--- src/main_app/models.py
+++ src/main_app/models.py
@@ -7,38 +7,35 @@ class stations(models.Model):
     id = models.CharField(primary_key=True)
     latitude = models.FloatField()
     longitude = models.FloatField()
-    code_postal = models.CharField()
-    adresse = models.CharField()
-    ville = models.CharField()
-    nom_departement = models.CharField()
-    code_departement = models.CharField()
-    nom_region = models.CharField()
-    code_region = models.CharField()
+    code_postal = models.CharField(null=True)
+    adresse = models.CharField(null=True)
+    ville = models.CharField(null=True)
+    nom_departement = models.CharField(null=True)
+    code_departement = models.CharField(null=True)
+    nom_region = models.CharField(null=True)
+    code_region = models.CharField(null=True)
+    automate_24_24 = models.BooleanField()
 
 
 class carburants(models.Model):
-    id = models.CharField(primary_key=True)
-    station_id = models.ForeignKey(stations, on_delete=models.CASCADE)
+    station_id = models.ForeignKey(stations, on_delete=models.CASCADE) # mettre nom station_id car la colonne se nomme station_id_id
     type_carburant = models.CharField()
-    prix = models.FloatField()
-    dat_maj = models.DateField()
+    prix = models.FloatField(null=True)
+    dat_maj = models.DateTimeField(null=True)
 
+    class Meta:
+        unique_together = ('station_id', 'type_carburant', 'prix', 'dat_maj')
 
 class ruptures(models.Model):
-    id = models.CharField(primary_key=True)
-    station_id = models.ForeignKey(stations, on_delete=models.CASCADE)
+    station_id = models.OneToOneField(stations, on_delete=models.CASCADE)
     type_carburant = models.CharField()
     rupture_debut = models.DateField()
     type_rupture = models.CharField()
 
 class services(models.Model):
-    id = models.CharField(primary_key=True)
-    station_id = models.ForeignKey(stations, on_delete=models.CASCADE)
-    service = models.CharField()
+    station_id = models.OneToOneField(stations, on_delete=models.CASCADE)
+    service = models.JSONField(null=True)  # Modifier en liste
 
-class horraires(models.Model):
-    id = models.CharField(primary_key=True)
-    station_id = models.ForeignKey(stations, on_delete=models.CASCADE)
-    jour = models.CharField()
-    horraires = models.CharField()
-    automate_24_24 = models.BooleanField()
+class horaires(models.Model):
+    station_id = models.OneToOneField(stations, on_delete=models.CASCADE)
+    horaires = models.JSONField()
