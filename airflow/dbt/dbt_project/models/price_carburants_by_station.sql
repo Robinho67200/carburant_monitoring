479d4eb1acb16edd5fb5403dfb3ad40380ea982f
Robin <jochem-robin@hotmail.fr>
2025-04-03 17:18:51 +0200

    Modification of the dbt query: price_carburants_by_station.sql
    Objective: Add a price of 0 for fuels not available at stations in order to display them on the search.html page.

--- airflow/dbt/dbt_project/models/price_carburants_by_station.sql
+++ airflow/dbt/dbt_project/models/price_carburants_by_station.sql
@@ -1,9 +1,31 @@
+WITH carburants_disponibles AS (
+    -- Liste des types de carburants disponibles
+    SELECT 'Diesel' AS type_carburant
+    UNION
+    SELECT 'Essence'
+    UNION
+    SELECT 'GPL'
+    UNION
+    SELECT 'SP95'
+    UNION
+    SELECT 'SP98'
+    UNION
+    SELECT 'E10'
+    UNION
+    SELECT 'E85'
+),
+stations_carburants_complets AS (
+    -- Créer toutes les combinaisons possibles de station_id et type_carburant
+    SELECT s.id AS station_id, c.type_carburant
+    FROM main_app_stations s
+    CROSS JOIN carburants_disponibles c
+)
 SELECT
-    station_id_id as station_id,
-    c.type_carburant,
-    CASE
-        WHEN c.prix IS NULL OR c.prix = 'NaN' THEN 0
-        ELSE c.prix
-    END AS prix
-FROM main_app_stations s
-LEFT JOIN main_app_carburants c ON c.station_id_id = s.id
+    s.station_id,
+    s.type_carburant,
+    COALESCE(c.prix, 0) AS prix
+FROM stations_carburants_complets s
+LEFT JOIN main_app_carburants c
+    ON s.station_id = c.station_id_id
+    AND s.type_carburant = c.type_carburant
+ORDER BY s.station_id, s.type_carburant
