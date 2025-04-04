70a4eeff98cf8693daeeb2c243680adc7113eae0
Robin <jochem-robin@hotmail.fr>
2025-04-04 23:06:01 +0200

    Modified the forms.py, search.html, base.html, and views.py files. Objective: Add a radius in kilometers to the input area so the user can refine their results.

--- src/main_app/forms.py
+++ src/main_app/forms.py
@@ -1,5 +1,10 @@
 from django import forms
 
-
 class AdresseForm(forms.ModelForm):
     adresse = forms.CharField(label="Adresse", max_length=450)
+    nb_km_max = forms.FloatField(
+        label="Distance maximale en km", min_value=5, max_value=40, initial=10
+    )
+
+    class Meta:
+        fields = ["adresse", "nb_km_max"]
