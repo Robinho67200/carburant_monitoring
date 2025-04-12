73977c3f4c28b90c61e51d1265930385c4d18bf2
Robin <jochem-robin@hotmail.fr>
2025-04-12 15:21:37 +0200

    Modification of the dbt file price_carburants_by_station. Objective: Fix a bug to only use the latest price for each fuel.

--- airflow/dbt/dbt_project/models/price_carburants_by_station.sql
+++ airflow/dbt/dbt_project/models/price_carburants_by_station.sql
@@ -14,18 +14,36 @@ WITH carburants_disponibles AS (
     UNION
     SELECT 'E85'
 ),
+
+ranking_by_station_carburant AS (
+    SELECT
+        station_id_id,
+        type_carburant,
+        prix,
+        dat_maj,
+        ROW_NUMBER() OVER (PARTITION BY station_id_id, type_carburant ORDER BY dat_maj DESC) AS rn
+    FROM main_app_carburants
+),
+
+last_station_carburant AS (
+SELECT *
+FROM ranking_by_station_carburant
+WHERE rn = 1
+),
+
 stations_carburants_complets AS (
     -- Créer toutes les combinaisons possibles de station_id et type_carburant
     SELECT s.id AS station_id, c.type_carburant
     FROM main_app_stations s
     CROSS JOIN carburants_disponibles c
 )
+
 SELECT
     s.station_id,
     s.type_carburant,
     COALESCE(c.prix, 0) AS prix
 FROM stations_carburants_complets s
-LEFT JOIN main_app_carburants c
+LEFT JOIN last_station_carburant c
     ON s.station_id = c.station_id_id
     AND s.type_carburant = c.type_carburant
 ORDER BY s.station_id, s.type_carburant
